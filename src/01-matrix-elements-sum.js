7adf6b05d22a8ae0cd191e5daabfbb3bf5a4d42b
zalutin-a <an9rey89@yandex.ru>
2021-04-06 08:49:35 +0300

    feat: implement task 01, 03

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,15 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  for (let i = 0; i < matrix[0].length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (matrix[j][i] <= 0) break;
+      sum += matrix[j][i];
+    }
+  }
+  return sum;
 }
 
 module.exports = getMatrixElementsSum;
--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,30 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const arr3 = [...arr];
+  const arr2 = [...arr];
+  function getI(ind, j) {
+    for (let i = ind; i <= j; i++) {
+      if (arr3[i] !== -1) return i;
+    }
+    return false;
+  }
+  for (let j = arr2.length - 1; j > 0; j--) {
+    for (let i = 0; i < j; i++) {
+      if (arr2[i] !== -1) {
+        if (arr2[i + 1] === -1) {
+          const ind2 = getI(i + 2, j);
+          if (arr2[i] > arr2[ind2]) {
+            [arr2[i], arr2[ind2]] = [arr2[ind2], arr2[i]];
+          }
+        } else if (arr2[i] > arr2[i + 1]) {
+          [arr2[i], arr2[i + 1]] = [arr2[i + 1], arr2[i]];
+        }
+      }
+    }
+  }
+  return arr2;
 }
 
 module.exports = sortByHeight;
