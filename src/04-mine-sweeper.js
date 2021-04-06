358a1b0e13e1947260818a0250e6127c34f147fc
zalutin-a <an9rey89@yandex.ru>
2021-04-06 15:33:36 +0300

    feat: implement task 4

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,29 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const matrix2 = new Array(matrix.length);
+  for (let index = 0; index < matrix2.length; index++) {
+    matrix2[index] = [];
+  }
+  function getMineCount(i, j) {
+    let count = 0;
+    for (let iz = i - 1; iz <= i + 1; iz++) {
+      for (let j2 = j - 1; j2 <= j + 1; j2++) {
+        if (iz >= 0 && j2 >= 0 && iz < matrix.length && j2 < matrix[0].length) {
+          if (iz === i && j2 === j); else if (!matrix[iz][j2]);
+          else { count++; }
+        }
+      }
+    }
+    return count;
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    matrix2[i] = [];
+    for (let j = 0; j < matrix[0].length; j++) {
+      matrix2[i].push(getMineCount(i, j));
+    }
+  }
+  return matrix2;
 }
-
 module.exports = minesweeper;
