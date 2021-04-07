b7caa1aa90e9495e409d4a4398f3688dd1ebb4a8
zalutin-a <an9rey89@yandex.ru>
2021-04-07 14:14:10 +0300

    feat: implement task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str;
+  const resArr = [];
+  function getCount(el, i) {
+    const n = 1;
+    if (arr[i] === arr[i + 1]) {
+      return n + getCount(arr[i + 2], i + 1);
+    }
+    return n;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    const n = getCount(arr[i], i);
+    resArr.push(n > 1 ? n + arr[i] : arr[i]);
+    i += n - 1;
+  }
+  return resArr.join('');
 }
 
 module.exports = encodeLine;
