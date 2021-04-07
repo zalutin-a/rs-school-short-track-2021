102da06cff4ca5261ee5970d3188873b2cfa276b
zalutin-a <an9rey89@yandex.ru>
2021-04-07 14:30:21 +0300

    refactor: delete excess strings in  task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,18 +8,17 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(str) {
-  const arr = str;
+function encodeLine(arr) {
   const resArr = [];
-  function getCount(el, i) {
+  function getCount(i) {
     const n = 1;
     if (arr[i] === arr[i + 1]) {
-      return n + getCount(arr[i + 2], i + 1);
+      return n + getCount(i + 1);
     }
     return n;
   }
   for (let i = 0; i < arr.length; i++) {
-    const n = getCount(arr[i], i);
+    const n = getCount(i);
     resArr.push(n > 1 ? n + arr[i] : arr[i]);
     i += n - 1;
   }
